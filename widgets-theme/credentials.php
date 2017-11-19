36660084a2ad0383b415a853615937fe42c76c71
mankita <m.ankita2431@gmail.com>
2017-11-18 16:02:58 -0800

    deleted conf file

--- widgets-theme/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-//credentials.php
-
-define('DB_NAME','ankitacodes_db');
-define('DB_USER','ankmis4');
-define('DB_PASSWORD','Pa55w0rd');
-define('DB_HOST','mysql.ankitacodes.dreamhosters.com');
